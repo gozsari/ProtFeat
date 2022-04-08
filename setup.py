22b8cf2c4cd9b957bab44d90489ec623a28f9316
GOKHAN OZSARI <gozsari@gmail.com>
2022-04-09 02:53:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 requirements = ['wget==3.2', 'requests==2.27.1', 'numpy==1.21.2']
 setuptools.setup(
     name='protFeat',
-    version='1.2',
+    version='1.0',
     author='Gokhan Ozsari',
     author_email='gozsari@ceng.metu.edu.tr',
     description='Installation of Package',
     long_description="readme",
     long_description_content_type="text/markdown",
-    url='https://github.com/gozsari/protFeat',
+    url='https://github.com/gozsari/ProtFeat',
     project_urls = {
-        "Bug Tracker": "https://github.com/gozsari/protFeat/issues"
+        "Bug Tracker": "https://github.com/gozsari/ProtFeat/issues"
     },
     install_requires=requirements,
     classifiers=[
